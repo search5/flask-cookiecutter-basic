8b7618e070c6a1ee7642b60f39005a34ab4e1aeb
ji-ho lee <search5@gmail.com>
2025-06-24 23:59:07 +0900

    directory struct change

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -2,8 +2,8 @@ from pathlib import Path
 
 project_name = '{{ cookiecutter.project_name }}'
 
-static_dir = Path(project_name) / "static"
-templates_dir = Path(project_name) / "templates"
+static_dir = "src" / Path(project_name) / "static"
+templates_dir = "src" / Path(project_name) / "templates"
 
 for item in (static_dir, templates_dir):
     item.mkdir()
